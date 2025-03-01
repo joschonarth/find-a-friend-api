96de160a9f65d36ffb4150fadf69951f02dc83f3
joschonarth <joschonarth@gmail.com>
2025-03-01 15:29:57 -0300

    feat: implement get pet controller

--- src/http/routes/pets.routes.ts
+++ src/http/routes/pets.routes.ts
@@ -3,7 +3,9 @@ import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 
 import { createPetController } from '../controllers/pets/create-pet.controller'
+import { getPetController } from '../controllers/pets/get-pet.controller'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', { onRequest: [verifyJwt] }, createPetController)
+  app.get('/orgs/pets/:id', getPetController)
 }
