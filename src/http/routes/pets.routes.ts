40ebc45da38a45154c368dda33dcf714f3f2d359
joschonarth <joschonarth@gmail.com>
2025-03-01 15:06:54 -0300

    feat: create verify jwt middleware

--- src/http/routes/pets.routes.ts
+++ src/http/routes/pets.routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { verifyJwt } from '../middlewares/verify-jwt'
+
 import { createPetController } from '../controllers/pets/create-pet.controller'
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post('/orgs/pets', createPetController)
+  app.post('/orgs/pets', { onRequest: [verifyJwt] }, createPetController)
 }
