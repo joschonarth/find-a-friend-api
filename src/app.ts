58992873c7f237522a096e1d7234b6e5f72bfbb2
joschonarth <joschonarth@gmail.com>
2025-03-01 10:58:43 -0300

    feat(auth): implement JWT authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
